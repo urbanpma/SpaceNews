80149895f416e134c62f1f22a761e1f518d976d4
UrbanPMA <urbanpma30@gmail.com>
2023-10-04 21:24:56 -0500

    Splash Theme 2

---
+++ app/src/main/java/com/ubnsoftware/spacenews/MainActivity.java
@@ -0,0 +1,31 @@
+package com.ubnsoftware.spacenews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Duración del splash screen en milisegundos
+    private static final int SPLASH_DURATION = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Duración del splash screen en milisegundos
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                // Este método se ejecutará una vez que transcurra el tiempo definido
+                // y nos llevará a la actividad principal
+                Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+                startActivity(intent);
+                finish(); // Cerrar la actividad actual
+            }
+        },SPLASH_DURATION);
+    }
+}
