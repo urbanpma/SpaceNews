5fde3291d0970dbe60b0c8fc8d16ccf07da44de0
UrbanPMA <urbanpma30@gmail.com>
2023-10-04 21:38:35 -0500

    Solución Abrir Theme

--- app/src/main/java/com/ubnsoftware/spacenews/MainActivity.java
+++ app/src/main/java/com/ubnsoftware/spacenews/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash_layout);
 
         // Duración del splash screen en milisegundos
         new Handler().postDelayed(new Runnable(){
